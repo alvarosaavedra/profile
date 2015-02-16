f180a64700da3c04e01ff56152117e65528c52d1
Alvaro Saavedra <asaabe@gmail.com>
2015-02-16 15:01:43 -0300

    cambios para que devise pueda ser usado en un engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Profile::Engine.routes.draw do
-	devise_for :users
+	devise_for :clients, class_name: "Profile::Client", module: :devise
 	resources :clients
 end
--- lib/profile/engine.rb
+++ lib/profile/engine.rb
@@ -1,3 +1,4 @@
+require 'devise'
 module Profile
   class Engine < ::Rails::Engine
     isolate_namespace Profile
